8f5cf8e5f795f886cb083a265b9a6e5c055cc3df
md-musa <mohammad.musa706@gmail.com>
2026-01-19 00:50:44 +0600

    fixed minor bugs

--- src/helper/sendEmail.ts
+++ src/helper/sendEmail.ts
@@ -1,27 +1,28 @@
 import nodemailer from "nodemailer";
 import config from "../config";
-
-
+import ApiError from "../errors/ApiError";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: config.EMAIL.USER,
-        pass: config.EMAIL.PASS
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: config.EMAIL.USER,
+    pass: config.EMAIL.PASS,
+  },
 });
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-    try {
-        await transporter.sendMail({
-            from: config.EMAIL.USER,
-            to,
-            subject,
-            html
-        });
-       // console.log(`Email sent to ${to}`);
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw new Error("Email could not be sent");
-    }
+  try {
+    await transporter.sendMail({
+      from: config.EMAIL.USER,
+      to,
+      subject,
+      html,
+    });
+    // console.log(`Email sent to ${to}`);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw ApiError.internal("Email could not be sent");
+  }
 };
